9448510c0005726310aaa0738906c95a83182409
Bryan Lilly <bryanjameslilly@gmail.com>
2022-03-27 19:47:41 +0100

    cors

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', '*');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header("Access-Control-Request-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
 

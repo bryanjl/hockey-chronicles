e1dcc91223180c47273273106d5fd31000acee07
Bryan Lilly <bryanjameslilly@gmail.com>
2022-03-27 11:52:54 +0100

    cors

--- server.js
+++ server.js
@@ -45,6 +45,14 @@ app.use(cors({
     origin: true
 }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 //body parser
 app.use(express.json());
 

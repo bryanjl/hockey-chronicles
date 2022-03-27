8635a322dce9ae1ff2d699615c53c909b7250f07
Bryan Lilly <bryanjameslilly@gmail.com>
2022-03-27 19:55:46 +0100

    cors

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use('/uploads', express.static('uploads'));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, DELETE, POST');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     res.header("Access-Control-Request-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();

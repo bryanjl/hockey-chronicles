8eb8a43109cdbbb151dfe4a68cc8bc670cfd84c4
Bryan Lilly <bryanjameslilly@gmail.com>
2022-03-28 15:55:59 +0100

    gameSearch query

--- controllers/game.js
+++ controllers/game.js
@@ -16,15 +16,13 @@ const Comment = require('../models/Comment');
 //@access   Public
 exports.getAllGames = asyncHandler(async (req, res, next) => {
     // res
-    
-        
-        res.writeHead(200, {
-            'Content-Type': 'application/json',
-            'Access-Control-Request-Headers': '*, Authorization',
-            'Access-Control-Allow-Headers': '*, Authorization',
-            'Access-Control-Allow-Methods': 'OPTIONS, GET, PUT, DELETE, POST, HEAD',
-            "Access-Control-Allow-Origin": 'https://hockey-chronicles-r3lzq.ondigitalocean.app'
-        })
+    res.setHeader("Access-Control-Allow-Origin", 'https://hockey-chronicles-r3lzq.ondigitalocean.app')
+    // res.header("Access-Control-Allow-Origin", '*');
+        .setHeader("Access-Control-Allow-Credentials", true)
+        .setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, DELETE, POST, HEAD')
+        .setHeader('Access-Control-Allow-Headers', '*, Authorization')
+        .setHeader('Access-Control-Request-Headers', '*, Authorization')
+        .writeHead(200, {'Content-Type': 'application/json'})
        
         .end(JSON.stringify(res.gameSearch));
         

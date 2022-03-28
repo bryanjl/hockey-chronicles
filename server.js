662ac354a6203cdf7d1cecc1764bf48e2863b0d5
Bryan Lilly <bryanjameslilly@gmail.com>
2022-03-28 12:04:41 +0100

    cors

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use('/uploads', express.static('uploads'));
 // }));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", ['https://hockey-chronicles-r3lzq.ondigitalocean.app', 'https://www.youtube.com']);
+    res.header("Access-Control-Allow-Origin", 'https://hockey-chronicles-r3lzq.ondigitalocean.app');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, DELETE, POST');
     res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept, application/json, Authorization');
